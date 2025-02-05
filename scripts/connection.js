60f48005d0fe721ee03774d7888bff0eca53dbf4
crosshair-xhr <crosshair.xhr@gmail.com>
2025-02-05 23:07:42 +0100

    Update connection.js

--- scripts/connection.js
+++ scripts/connection.js
@@ -1,13 +1,17 @@
 const sleep = ms => new Promise(r => setTimeout(r, ms));
-const print = console.log
+const print = console.log;
 
-while(true) {
-    sleep(100);
+async function main() {
+    while (true) {
+        await sleep(100);
 
-    try {
-        var result = fetch("http://127.0.0.1:5304");
-        print(result);
-    } catch(error) {
-        print(error);
+        try {
+            const result = await fetch("http://127.0.0.1:5304");
+            print(result);
+        } catch (error) {
+            print(error);
+        }
     }
 }
+
+main();
