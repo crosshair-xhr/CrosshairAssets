2f9947d443df579b00379f780c12d99cc8ce6300
crosshair-xhr <crosshair.xhr@gmail.com>
2025-02-06 14:37:40 +0100

    Update connection.js

--- scripts/connection.js
+++ scripts/connection.js
@@ -4,15 +4,11 @@ async function main() {
     while (true) {
         await sleep(1000);
 
-        try {
-            endpoint = `http://127.0.0.1:${PORT}/api/status`
+        var endpoint = `http://127.0.0.1:${PORT}/api/status`
 
-            const result = await fetch();
-            print("Recieved status, Online...");
-            print(status)
-        } catch (error) {
-            alert("error");
-        }
+        const result = await fetch();
+        print("Recieved status, Online...");
+        print(status)
     }
 }
 
