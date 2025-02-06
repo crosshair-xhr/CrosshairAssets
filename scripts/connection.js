cca403ec2db513d952f7a435fe1d5b011e3ce0f5
crosshair-xhr <crosshair.xhr@gmail.com>
2025-02-06 18:23:47 +0100

    Update connection.js

--- scripts/connection.js
+++ scripts/connection.js
@@ -1,14 +1,24 @@
 const PORT = 8890;
 
 async function main() {
+    const status_endpoint = `http://127.0.0.1:${PORT}/api/status`
+
     while (true) {
         await sleep(1000);
 
-        var endpoint = `http://127.0.0.1:${PORT}/api/status`
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), 3000);
 
-        const result = await fetch(endpoint);
-        print("Recieved status, Online...");
-        print(status)
+        try {
+            var result = await fetch(status_endpoint);
+            connection_status = "online"
+        } catch {
+            connection_status = "offline"
+        } finally {
+            clearTimeout(timeoutId)
+        }
+
+        document.getElementById("status").textContent = connection_status.toUpperCase()
     }
 }
 
