0aa70ecca4e59ad753436f97a23f5407c0a88f31
crosshair-xhr <crosshair.xhr@gmail.com>
2025-02-10 07:14:55 +0100

    Update close.js

--- scripts/close.js
+++ scripts/close.js
@@ -2,6 +2,7 @@ async function main() {
     var close_button = await waitForElement("close-btn");
 
     close_button.addEventListener("click", async () => {
+        console.log(`${BRIDGE}/api/close`);
         await fetch(`${BRIDGE}/api/close`);
         window.close();
     });
