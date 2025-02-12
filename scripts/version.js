88b49a1363eadea69220a3fb10b21e9e0757824e
crosshair-xhr <crosshair.xhr@gmail.com>
2025-02-12 07:48:26 +0100

    Update version.js

--- scripts/version.js
+++ scripts/version.js
@@ -1,5 +1,5 @@
 async function main() {
-    var version = document.getElementById("version");
+    var version = waitForElement("version");
     var available_version = await (await fetch("https://crosshair-xhr.github.io/CrosshairAssets/version")).text();
     var current_version = await (await fetch(`${BRIDGE}/api/status`)).text();
 
