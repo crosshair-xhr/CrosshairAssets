b3f6180b4da494662a0196fc33ecc9b8a8fc1f95
crosshair-xhr <crosshair.xhr@gmail.com>
2025-02-13 07:38:56 +0100

    Update version.js

--- scripts/version.js
+++ scripts/version.js
@@ -4,6 +4,10 @@ async function main() {
     var current_version = await (await fetch(`${BRIDGE}/api/status`)).text();
 
     version.textContent = current_version;
+
+    if (available_version != current_version) {
+        version.style.color = "#ff0000"
+    }
 };
 
 main();
